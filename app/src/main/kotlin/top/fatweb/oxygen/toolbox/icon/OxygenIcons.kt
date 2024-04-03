58117cc6f6bf3d30a765e58a8a2216d848e8dea4
FatttSnake <fatttsnake@gmail.com>
2024-04-03 18:08:48 +0800

    Refactor(Icons): Add icons
    Add icons and optimize sorting

--- app/src/main/kotlin/top/fatweb/oxygen/toolbox/icon/OxygenIcons.kt
+++ app/src/main/kotlin/top/fatweb/oxygen/toolbox/icon/OxygenIcons.kt
@@ -1,11 +1,15 @@
 package top.fatweb.oxygen.toolbox.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.Inbox
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Reorder
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material.icons.rounded.Home
@@ -14,17 +18,19 @@ import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Star
 
 object OxygenIcons {
+    val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+    val ArrowDown = Icons.Rounded.KeyboardArrowDown
+    val Back = Icons.Rounded.ArrowBackIosNew
+    val Box = Icons.Default.Inbox
+    val Code = Icons.Default.Code
     val Home = Icons.Rounded.Home
     val HomeBorder = Icons.Outlined.Home
+    val Info = Icons.Outlined.Info
+    val MoreVert = Icons.Default.MoreVert
+    val Reorder = Icons.Default.Reorder
+    val Search = Icons.Rounded.Search
     val Star = Icons.Rounded.Star
     val StarBorder = Icons.Outlined.StarBorder
-    val Search = Icons.Rounded.Search
-    val MoreVert = Icons.Default.MoreVert
-    val Back = Icons.Rounded.ArrowBackIosNew
-
-    val ArrowDown = Icons.Rounded.KeyboardArrowDown
-
-    val Box = Icons.Default.Inbox
-    val Tool = Icons.Default.Build
     val Time = Icons.Default.AccessTime
+    val Tool = Icons.Default.Build
 }
